a6ba0e6043ed882010756560b78d8a6337e38958
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-01 19:30:02 -0800

    commit tree c29f34c88819444ab96f37e2a997af63300cacb5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = []
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice((drivers.length - 2), (drivers.length))
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(num1) {
+    return function (num2) {
+        return num1 * num2
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers)
+}
+
